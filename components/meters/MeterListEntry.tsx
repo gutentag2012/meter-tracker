e95dd25d9179d703f60be79893fc9fd366ddc0c9
Joshua Gawenda <joshua.gawenda@gmail.com>
2023-08-16 20:27:09 +0200

    Display difference from last entry instead of current value

--- components/meters/MeterListEntry.tsx
+++ components/meters/MeterListEntry.tsx
@@ -30,25 +30,48 @@ export const MeterListEntry: FunctionComponent<Props> = ({
   // Should not display the add button, if there is already an entry for today
   const hasButton = !!navigateToAddMeasurement && !moment()
     .startOf('day')
-    .isBefore(moment(meter.lastMeasurementDate).endOf("day"))
+    .isBefore(moment(meter.lastMeasurements?.[0].date).endOf("day"))
 
   const AddButtonOffset = hasButton ? 16 : -8
   const styles = makeStyles(AddButtonOffset)
 
   const subTitle = useMemo(() => {
-    if (!meter.lastMeasurementDate) {
+    if (!meter.lastMeasurements?.[0].date) {
       return t('meter:no_previous_reading')
     }
 
-    const formattedLastMeasurementDate = moment(meter.lastMeasurementDate)
+    const formattedLastMeasurementDate = moment(meter.lastMeasurements?.[0].date)
       .format('LL')
     return t('meter:last_reading', { date: formattedLastMeasurementDate })
-  }, [meter.lastMeasurementDate])
+  }, [meter.lastMeasurements])
 
-  const formattedValue = useMemo(() => t('meter:reading_value', {
-    value: parseValueForDigits(meter.lastMeasurementValue ?? 0, meter.digits),
-    unit: meter.unit,
-  }), [meter])
+  const percentileChange = useMemo(() => {
+    if ((meter.lastMeasurements?.length ?? 0) < 3) {
+      return 0
+    }
+    const date1 = moment(meter.lastMeasurements?.[0].date)
+    const date2 = moment(meter.lastMeasurements?.[1].date)
+    const date3 = moment(meter.lastMeasurements?.[2].date)
+    const value1 = meter.lastMeasurements?.[0].value ?? 0
+    const value2 = meter.lastMeasurements?.[1].value ?? 0
+    const value3 = meter.lastMeasurements?.[2].value ?? 0
+
+    const daysBetween1 = date1.endOf('day').diff(date2.startOf('day'), 'days') || 1
+    const daysBetween2 = date2.endOf('day').diff(date3.startOf('day'), 'days') || 1
+
+    const delta1 = (value1 - value2) / daysBetween1
+    const delta2 = (value2 - value3) / daysBetween2
+
+    return (delta1 - delta2) / Math.abs(delta2 || 1) * 100
+  }, [meter])
+
+  const areValuesGood = percentileChange !== undefined && ((percentileChange >= 0 && !!meter.areValuesDepleting) || (percentileChange <= 0 && !meter.areValuesDepleting))
+
+  // TODO Add meter modal allow comma
+  // TODO Contract, add yearly base price
+  // TODO Contract, add unit conversion
+  // TODO Add yearly stats
+  // TODO Fix issue with contract last months price calucaltion
 
   return <>
     <Ripple
@@ -77,10 +100,10 @@ export const MeterListEntry: FunctionComponent<Props> = ({
         </Text>
       </View>
       <Text
-        style={ styles.value }
+            style={ [styles.value, { color: areValuesGood ? Colors.tertiary : Colors.error }] }
         onSurface
       >
-        { formattedValue }
+          { percentileChange > 0 && '+' }{ parseValueForDigits(percentileChange, 2) }%
       </Text>
     </Ripple>
     {
