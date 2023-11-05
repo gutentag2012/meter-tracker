e18a4aa983087edc33dcb5c5d612a03a2871e50f
Joshua Gawenda <joshua.gawenda@gmail.com>
2023-11-05 11:53:20 +0100

    Add utils for listening to asnc storage value

--- utils/CommonUtils.ts
+++ utils/CommonUtils.ts
@@ -1,1 +1,30 @@
+import { useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import EventEmitter from '../services/events'
+
 export const Truthy = <T>(x: T | null | undefined): x is T => !!x
+
+export const useAsyncStorageValue = <T>(key: string): T | undefined => {
+  const [value, setValue] = useState<T | undefined>()
+
+  useEffect(() => {
+    AsyncStorage.getItem(key).then((value) => {
+      if (!value) {
+        setValue(undefined)
+        return
+      }
+
+      setValue(JSON.parse(value))
+    })
+
+    const onValueChange = (v: T | undefined) => {
+      console.log('Changed Async Value', key, v)
+      setValue(v)
+    }
+
+    EventEmitter.subscribe(`settings-${key}`, onValueChange)
+    return () => EventEmitter.unsubscribe(`settings-${key}`, onValueChange)
+  }, [key])
+
+  return value
+}
