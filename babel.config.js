d288c4f0469dbed92445288f67f80d8d5ca58cf2
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-03-23 16:49:47 +0100

    chore: add back signals

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       ['inline-import', { extensions: ['.sql'] }],
+      'module:@preact/signals-react-transform',
       'react-native-reanimated/plugin',
     ],
   }
