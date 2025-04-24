ceb7354a49b19feef5bed768ddfbe194dd769e64
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-04-24 17:00:32 +0200

    fix: tax not loading on start

--- modules/settings/tax.signals.ts
+++ modules/settings/tax.signals.ts
@@ -1,11 +1,8 @@
-import { getLocales } from 'expo-localization'
 import { signal } from '@preact/signals-core'
 import { effect } from '@preact/signals-react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { reloadAppAsync } from 'expo'
 import { StorageKeys } from '@/modules/general/constants'
 import { numberOfSettingsLoaded } from './settings.signals'
-import { changeLocale, translate } from '@/modules/general/translations'
 
 export const tax = signal(0)
 
